5db8b3a19985a10a53a6652bb9590efbd4753e1f
miledch <miledch@gmail.com>
2020-11-18 18:25:20 -0500

    MemberDashboard_V1
    first commit of the member dashboard

--- MemberDashboard/MemberDashboard.php
+++ MemberDashboard/MemberDashboard.php
@@ -4,34 +4,45 @@
 		<title>Member Dashboard</title>
 		
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+		<link rel="stylesheet" href="Style.css">
+		<script src='https://kit.fontawesome.com/a076d05399.js'></script>
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 	</head>
 	
 	<body style="background-color:#DDDFEB">
-		
-		<div>
-	
-			<?php
+		<div class=container style="width: 50em;">
+			<div id="divID">
+				<p id="pID"> <span style='font-size:50px;'>&#128100;</span> name of post owner</p>
+				</br>
+				<h5 id="h1ID"> 
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris at nunc vitae arcu dapibus ultrices et eu sem. Ut suscipit, tortor vel facilisis vestibulum, tellus tortor convallis purus, vel aliquet nibh sem at ligula. Etiam id massa ipsum. Mauris pharetra est eget malesuada hendrerit. Vivamus dapibus facilisis justo id faucibus. Fusce iaculis consequat viverra. Nullam aliquet maximus eros at maximus. Curabitur nec sem eget massa auctor dapibus. Sed luctus dignissim dictum. Maecenas varius sapien at odio lobortis finibus.
+				</h5>
+				<img id="imageID" src="image1.jpg"> </img>
+				</br></br>
+				<button style='font-size:24px'>Comment <i class='far fa-comment'></i></button>
+			</div>
+			<div id="divID">
+				<p id="pID"> <span style='font-size:50px;'>&#128100;</span> name of post owner</p>
+				</br>
+				<h5 id="h1ID"> 
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris at nunc vitae arcu dapibus ultrices et eu sem. Ut suscipit, tortor vel facilisis vestibulum, tellus tortor convallis purus, vel aliquet nibh sem at ligula. Etiam id massa ipsum. Mauris pharetra est eget malesuada hendrerit. Vivamus dapibus facilisis justo id faucibus. Fusce iaculis consequat viverra. Nullam aliquet maximus eros at maximus. Curabitur nec sem eget massa auctor dapibus. Sed luctus dignissim dictum. Maecenas varius sapien at odio lobortis finibus.
+				</h5>
+				<img id="imageID" src="image1.jpg"> </img>
+				</br></br>
+				<button style='font-size:24px'>Comment <i class='far fa-comment'></i></button>
+			</div>
+			<div id="divID">
+				<p id="pID"> <span style='font-size:50px;'>&#128100;</span> name of post owner</p>
+				</br>
+				<h5 id="h1ID"> 
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris at nunc vitae arcu dapibus ultrices et eu sem. Ut suscipit, tortor vel facilisis vestibulum, tellus tortor convallis purus, vel aliquet nibh sem at ligula. Etiam id massa ipsum. Mauris pharetra est eget malesuada hendrerit. Vivamus dapibus facilisis justo id faucibus. Fusce iaculis consequat viverra. Nullam aliquet maximus eros at maximus. Curabitur nec sem eget massa auctor dapibus. Sed luctus dignissim dictum. Maecenas varius sapien at odio lobortis finibus.
+				</h5>
+				<img id="imageID" src="image1.jpg"> </img>
+				</br></br>
+				<button style='font-size:24px'>Comment <i class='far fa-comment'></i></button>
+			</div>
 			
-				session_start();
-				
-				
-				$servername = "gzc353.encs.concordia.ca";
-				$username = "gzc353_2";
-				$password = "B3NGRy";
-				$num_rows;
-
-				try 
-				{
-					$conn = new PDO("mysql:host=$servername;dbname=gzc353_2", $username, $password);
-					// set the PDO error mode to exception
-					$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-					
-					$memberID = $_SESSION['memberID'];
-
-					DISPLAY THE MEMBER POST HERE
-
-
+		</div>
 	</body>
 
 </html>
