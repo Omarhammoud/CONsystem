ef59126b20258436fd981977ed106d6bc7c2ecce
miledch <miledch@gmail.com>
2020-11-30 00:27:37 -0500

    changing the insertPost

--- MemberDashboard/insertPost.php
+++ MemberDashboard/insertPost.php
@@ -1,20 +1,20 @@
 <?php
-    
-    $dbServername = "gzc353.encs.concordia.ca";
-$dbUsername = "gzc353_2";
-$dbPassword = "B3NGRy"; // default root, nothing for password for xxamp
+
+$dbServername = "localhost";
+$dbUsername = "root";
+$dbPassword = ""; // default root, nothing for password for xxamp
 $dbName = "gzc353_2";
 
-$conn = mysqli_connect($dbServername,$dbUsername,$dbPassword,$dbName);
+$conn = mysqli_connect($dbServername,$dbUsername,$dbPassword,$dbName) or die("could not connect");
     $text = $_POST["text"];
     $img = $_POST["img"];
     $pollTitle = $_POST["pollTitle"];
     $pollOpt = $_POST["pollOpt"];
     $numOfPollOpts = count($pollOpt);
-    $currDate = date("Y-m-d");
+    $currDate = CURDATE();
 
-    $sql = "INSERT INTO content 
-            VALUES ('1', '$text', '$currDate');";
+    $sql = "INSERT INTO content (MemberID, ContentBody, Type, date)  
+            VALUES (1, $text, 'public', $currDate);";
     mysqli_query($conn, $sql);
     header("Location: ./NewPost.html");
 
