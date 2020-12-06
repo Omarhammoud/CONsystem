8daefebaa3d878bad530561cdbcad227e0378cda
omarhammoud <omar_hammoud96@hotmail.com>
2020-12-05 22:49:18 -0500

    fixed issues with user edit and delete

--- includes/UserEdit.inc.php
+++ includes/UserEdit.inc.php
@@ -9,7 +9,7 @@ if(isset($_GET['MemberID1'])) {
     $sql = "SELECT Email, Name, Address, Status, Privilege FROM member WHERE MemberID = ? ";
     $stmt = mysqli_stmt_init($conn);
     if (!mysqli_stmt_prepare($stmt, $sql)) {
-        header("Location: ../includes/UserEdit.inc.php?error=sqlerror1");
+        header("Location: ./UserEdit.inc.php?error=sqlerror1");
         exit();
     } else {
         mysqli_stmt_bind_param($stmt, "i", $memberID);
