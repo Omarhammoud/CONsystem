4008eb47003c4bfde84cdb36e7c0ee2c46011b99
omarhammoud <omar_hammoud96@hotmail.com>
2020-12-13 17:11:32 -0500

    fixed db connection info

--- includes/dbh.inc.php
+++ includes/dbh.inc.php
@@ -5,15 +5,11 @@
 <?php
 
 //Database Handler
- $servername = "localhost";
- $dBUsername = "root";
- $dBPassword = "";
- $dBName = "Consys";
 
-//$servername = "gzc353.encs.concordia.ca";
-//$dBUsername = "gzc353_2";
-//$dBPassword = "B3NGRy";
-//$dBName = "gzc353_2";
+$servername = "gzc353.encs.concordia.ca";
+$dBUsername = "gzc353_2";
+$dBPassword = "B3NGRy";
+$dBName = "gzc353_2";
 
 
 $conn = mysqli_connect($servername, $dBUsername, $dBPassword,$dBName );
